aaa5223b9c6d2b95670c64262b7071fa55e7414b
Taha Sari <saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.com>
2022-10-31 00:18:50 +0300

    commit2-bugsfixed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner=new Scanner(System.in);
+        int number,n1=0,n2=0,n3;
+        System.out.println("Please enter a number:");
+        number= scanner.nextInt();
+        for(int i=0;i<=number;i++){
+            n3=n1+n2;
+            System.out.print(" "+n3);
+            if(n2==0){n2++;}
+            n1=n2;
+            n2=n3;
+
+
+
+        }
+
+System.out.println();
+        }
     }
-}
